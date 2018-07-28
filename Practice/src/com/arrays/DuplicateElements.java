4f4a2894cbe31e4f630b58fca4e87cf5d9f88192
karthik <karthik@karthik-Latitude-3540>
2018-07-28 09:53:43 +0530

    DuplicateElements.java

---
+++ Practice/src/com/arrays/DuplicateElements.java
@@ -0,0 +1,35 @@
+package com.arrays;
+
+public class DuplicateElements {
+
+	public static void main(String args[])
+	{
+	
+		int[] array= {1,2,3,4,2};
+		System.out.println(duplicateElements(array));
+		
+	}
+
+	private static int duplicateElements(int[] array) {
+		
+		for(int i=0;i<array.length;i++)
+		{
+			if(array[i]<=0 || array[i]>=array.length)
+			{
+				return -1; //If input is wrong
+			}
+			if(array[array[i]-1]!=-1)
+			{
+				array[array[i]-1]=-1;
+			}
+			else
+			{
+				return array[i];
+			}
+		}
+		
+		return -1;
+	}	
+
+	
+}
